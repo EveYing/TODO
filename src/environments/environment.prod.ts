f82b685fbc64f65af509766c9591b6cab11949b3
Eve Ying <eveying96@gmail.com>
2020-10-25 20:25:54 -0400

    fix typo in prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
-  apiKey: "AIzaSyBLjIItKdd4qW0sX3KnJFw9lu3osGIe6mI",
-  authDomain: "to-do-list-8d987.firebaseapp.com",
-  databaseURL: "https://to-do-list-8d987.firebaseio.com",
-  projectId: "to-do-list-8d987",
-  storageBucket: "to-do-list-8d987.appspot.com",
-  messagingSenderId: "9750174811",
-  appId: "1:9750174811:web:b90b5d4a9179f8414af37e",
-  measurementId: "G-45Q4E6945L"
-}
+  firebase: {
+    apiKey: 'AIzaSyBLjIItKdd4qW0sX3KnJFw9lu3osGIe6mI',
+    authDomain: 'to-do-list-8d987.firebaseapp.com',
+    databaseURL: 'https://to-do-list-8d987.firebaseio.com',
+    projectId: 'to-do-list-8d987',
+    storageBucket: 'to-do-list-8d987.appspot.com',
+    messagingSenderId: '9750174811',
+    appId: '1:9750174811:web:b90b5d4a9179f8414af37e',
+    measurementId: 'G-45Q4E6945L',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBLjIItKdd4qW0sX3KnJFw9lu3osGIe6mI",
-    authDomain: "to-do-list-8d987.firebaseapp.com",
-    databaseURL: "https://to-do-list-8d987.firebaseio.com",
-    projectId: "to-do-list-8d987",
-    storageBucket: "to-do-list-8d987.appspot.com",
-    messagingSenderId: "9750174811",
-    appId: "1:9750174811:web:b90b5d4a9179f8414af37e",
-    measurementId: "G-45Q4E6945L"
-  }
+    apiKey: 'AIzaSyBLjIItKdd4qW0sX3KnJFw9lu3osGIe6mI',
+    authDomain: 'to-do-list-8d987.firebaseapp.com',
+    databaseURL: 'https://to-do-list-8d987.firebaseio.com',
+    projectId: 'to-do-list-8d987',
+    storageBucket: 'to-do-list-8d987.appspot.com',
+    messagingSenderId: '9750174811',
+    appId: '1:9750174811:web:b90b5d4a9179f8414af37e',
+    measurementId: 'G-45Q4E6945L',
+  },
 };
 
 /*
